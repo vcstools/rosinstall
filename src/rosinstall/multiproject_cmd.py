aa9c089a76803b5a1feb937532fb1c8aef629599
Thibault Kruse <kruset@in.tum.de>
2012-02-23 19:40:23 -0800

    missing files

---
+++ src/rosinstall/multiproject_cmd.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+import pkg_resources
+
+import config_yaml
+from common import MultiProjectException
+from config import Config
+from config_yaml import aggregate_from_uris
+
+
+## The _cmd python files attempt to provide a reasonably
+## complete level of abstraction to multiproject functionality.
+## 
+## Client code will need to pass the Config element through,
+## and may use the ConfigElement API in places.
+## There are no guarantees at this time for the API to
+## remain stable, but the cmd API probably will change least.
+## A change to expect is abstraction of user interaction.
+
+import vcstools
+from vcstools import VcsClient
+
+def get_config(basepath, config_uris, config_filename):
+  """
+  Create a Config element necessary for all other commands.
+  The command will look at the uris in sequence, each
+  can be a web resource, a filename or a folder. In case it is
+  a folder, when a config_filename is provided, the folder will
+  be searched for a file of that name, and that one will be used.
+  Else the folder will be considered a target location for the config.
+  All files will be parsed for config elements, thus conceptually
+  the input to Config is an expanded list of config elements. Config
+  takes this list and consolidates duplicate paths by keeping the last
+  one in the list.
+  :param basepath: where relative paths shall be resolved against
+  :param config_uris: the location of config specifications or folders
+  :param config_filname: name of files which may be looked at for config information
+  :returns: a Config object
+  :raises MultiProjectException: on plenty of errors
+  """
+  # Find all the configuration sources
+
+  aggregate_source_yaml = aggregate_from_uris(config_uris, basepath, config_filename)
+    
+  ## Could not get uri therefore error out
+  if len(config_uris) == 0:
+    parser.error("no source config file found! looked at arguments and, %s"%(
+        os.path.join(basepath, config_filename)))
+
+  #print("source...........................", aggregate_source_yaml)
+
+  ## Generate the config class with the uri and path
+  config = Config(aggregate_source_yaml, basepath, config_filename)
+
+  return config
+
+
+def cmd_persist_config(config, filename, header):
+    """writes config to given file in yaml syntax"""
+    config_yaml.generate_config_yaml(config, filename, header)
+
+    
+def cmd_version():
+  """Returns extensive version information"""
+  def prettyversion(vdict):
+    version = vdict.pop("version")
+    return "%s; %s"%(version, ",".join(vdict.values()) )
+  return """vcstools:  %s
+SVN:       %s
+Mercurial: %s
+Git:       %s
+Tar:       %s
+Bzr:       %s
+"""%(pkg_resources.require("vcstools")[0].version,
+     prettyversion(vcstools.SvnClient.get_environment_metadata()),
+     prettyversion(vcstools.HgClient.get_environment_metadata()),
+     prettyversion(vcstools.GitClient.get_environment_metadata()),
+     prettyversion(vcstools.TarClient.get_environment_metadata()),
+     prettyversion(vcstools.BzrClient.get_environment_metadata()))
+
+def cmd_status(config, path, untracked = False):
+  """
+  calls SCM status for all SCM entries in config, relative to path
+  :returns: List of dict {element: ConfigElement, diff: diffstring}
+  :param untracked: also show files not added to the SCM
+  :raises MultiProjectException: on plenty of errors
+  """
+  result=[]
+  for element in config.get_config_elements():
+    path_spec = element.get_path_spec()
+    if element.is_vcs_element():
+      scmtype = path_spec.get_scmtype()
+      status = element.get_status(path, untracked)
+      # align other scm output to svn
+      columns = -1
+      if scmtype == "git":
+        columns = 3
+      elif scmtype == "hg":
+        columns = 2
+      elif scmtype == "bzr":
+        columns = 4
+      if columns > -1 and status != None:
+        status_aligned = ''
+        for line in status.splitlines():
+          status_aligned = status_aligned + line[:columns].ljust(8) + line[columns:] + '\n'
+        status = status_aligned
+      result.append({'entry':element, 'status':status})
+  return result
+
+
+def cmd_diff(config, path):
+  """
+  calls SCM diff for all SCM entries in config, relative to path
+  :returns: List of dict {element: ConfigElement, diff: diffstring}
+  :raises MultiProjectException: on plenty of errors
+  """
+  result=[]
+
+  for element in config.get_config_elements():
+    if element.is_vcs_element():
+      result.append({'entry':element, 'diff':element.get_diff(path)})
+  return result
+
+def cmd_install_or_update(config, backup_changed, mode, robust):
+    """
+    performs many things, generally attempting to make
+    the local filesystem look like what the config specifies,
+    pulling from remote sources the most recent changes.
+    
+    The command may have stdin user interaction (TODO abstract)
+    :param backup_changed: whether 
+    :returns: True on Success
+    :raises MultiProjectException: on plenty of errors
+    """
+    return config.execute_install(backup_changed, mode, robust)
---
+++ src/rosinstall/rosinstall_cmd.py
@@ -0,0 +1,58 @@
+import subprocess
+import multiproject_cmd
+import rosinstall.setupfiles
+from rosinstall.helpers import ROSInstallException, __ROSINSTALL_FILENAME
+
+def cmd_persist_config_file(config):
+  ## Save .rosinstall
+  header = """# THIS IS A FILE WHICH IS MODIFIED BY rosinstall
+# IT IS UNLIKELY YOU WANT TO EDIT THIS FILE BY HAND,
+# UNLESS FOR REMOVING ENTRIES.
+# IF YOU WANT TO CHANGE THE ROS ENVIRONMENT VARIABLES
+# USE THE rosinstall TOOL INSTEAD.
+# IF YOU CHANGE IT, USE rosinstall FOR THE CHANGES TO TAKE EFFECT
+"""
+  multiproject_cmd.cmd_persist_config(config, __ROSINSTALL_FILENAME, header)
+
+def _ros_requires_boostrap(config):
+  """Whether we might need to bootstrap ros"""
+  for entry in config.get_source():
+    if rosinstall.helpers.is_path_ros(entry.get_path()):
+      # we assume that if any of the elements we installed came
+      # from a VCS source, a bootsrap might be useful
+      if entry.get_scmtype() is not None:
+        return True
+  return False
+  
+def cmd_generate_ros_files(config, path, nobuild, rosdep_yes, catkin, catkinpp):
+  """
+  Generates ROS specific setup files
+  """
+ 
+  ## bootstrap the build if installing ros
+  if catkin:
+    with open(os.path.join(path, "CMakeLists.txt"), 'w') as cmake_file:
+      cmake_file.write(CATKIN_CMAKE_TOPLEVEL)
+
+    if catkinpp:
+      with open(os.path.join(path, "workspace-config.cmake"), 'w') as config_file:
+        config_file.write("set (CMAKE_PREFIX_PATH %s)"%catkinpp)
+    
+                
+  else: # DRY install case
+    ## Generate setup.sh and save
+    rosinstall.setupfiles.generate_setup(config)
+
+    if _ros_requires_boostrap(config) and not nobuild:
+      print("Bootstrapping ROS build")
+      rosdep_yes_insert = ""
+      if rosdep_yes:
+        rosdep_yes_insert = " --rosdep-yes"
+      ros_comm_insert = ""
+      if 'ros_comm' in [os.path.basename(tree.path) for tree in config.trees]:
+        print("Detected ros_comm bootstrapping it too.")
+        ros_comm_insert = " ros_comm"
+      subprocess.check_call("source %s && rosmake ros%s --rosdep-install%s" % (os.path.join(path, 'setup.sh'), ros_comm_insert, rosdep_yes_insert), shell=True, executable='/bin/bash')
+    print("\nrosinstall update complete.\n\nNow, type 'source %s/setup.bash' to set up your environment.\nAdd that to the bottom of your ~/.bashrc to set it up every time.\n\nIf you are not using bash please see http://www.ros.org/wiki/rosinstall/NonBashShells " % path)
+  
+    
