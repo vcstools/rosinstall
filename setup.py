4ab8bc1cc633ff1dc887924483735a8b1055e293
Tully Foote <tfoote@willowgarage.com>
2012-03-07 17:20:52 -0800

    upgrading to 0.6.0 of rosinstall

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except:
 
 
 setup(name='rosinstall',
-      version= '0.5.30',
+      version= '0.6.0',
       packages=['rosinstall'],
       package_dir = {'':'src'},
       install_requires = ['vcstools'],
