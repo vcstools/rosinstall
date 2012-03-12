b3472d35fd6c9341f017c502d04783bdb75fb68a
Tully Foote <tfoote@willowgarage.com>
2012-03-12 11:56:23 -0700

    commenting data files due to bad implementation in setuptools and going to 0.6.5

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ except:
 
 
 setup(name='rosinstall',
-      version= '0.6.4',
+      version= '0.6.5',
       packages=['rosinstall'],
       package_dir = {'':'src'},
       install_requires = ['vcstools'],
       scripts = ["scripts/rosinstall", "scripts/roslocate", "scripts/py-rosws", "scripts/rosco"],
-      data_files=[('/etc/bash_completion.d', ['contrib/rosws.bash']),
-                  ('/etc/bash_completion.d', ['contrib/rosws.shell']),
-                  ('/etc/bash_completion.d', ['contrib/rosinstall.bash'])],
+      #data_files=[('/etc/bash_completion.d', ['contrib/rosws.bash']),
+      #            ('/etc/bash_completion.d', ['contrib/rosws.shell']),
+      #            ('/etc/bash_completion.d', ['contrib/rosinstall.bash'])],
       author = "Tully Foote", 
       author_email = "tfoote@willowgarage.com",
       url = "http://www.ros.org/wiki/rosinstall",
