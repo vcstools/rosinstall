4beed297052cdcc366f0b72535f3c31c7c1b373e
Thibault Kruse <kruset@in.tum.de>
2013-01-26 12:25:23 +0100

    refactor and fix tests, assumed ros had to be in svn

--- test/network/distro_test_util.py
+++ test/network/distro_test_util.py
@@ -4,29 +4,27 @@ def ros_found_in_yaml(yaml_elements):
     '''
     ros_found = False
     print(yaml_elements)
-    for e in yaml_elements:
+    for elt in yaml_elements:
         scm_found = False
         for scm in ['svn', 'tar', 'git', 'hg']:
-            if scm in e:
+            if scm in elt:
                 scm_found = True
-                element = e[scm]
-                if "local-name" in element:
-                    if element["local-name"] == "ros":
-                        ros_found = True
-                        break
+                element = elt[scm]
+                if element.get("local-name", "") == "ros":
+                    ros_found = True
+                    break
         if not scm_found:
-            element = e.get("other", '')
-            if "local-name" in element:
-                if element["local-name"].endswith('/ros'):
+            if "other" in elt:
+                element = elt["other"]
+                if element.get("local-name", "").endswith('/ros'):
                     ros_found = True
     return ros_found
 
 
 def ros_found_in_path_spec(specs):
     ros_found = False
-    for s in specs:
-        if "svn" == s.get_scmtype() or "tar" == s.get_scmtype() or s.get_scmtype() is None:
-            if s.get_path().endswith('ros'):
-                ros_found = True
-                break
+    for spec in specs:
+        if spec.get_path().endswith('ros'):
+            ros_found = True
+            break
     return ros_found
