aa82d6f4fe6f0db08e0f8079cffe1781d4068f18
Dirk Thomas <dthomas@osrfoundation.org>
2013-09-18 15:48:47 -0700

    add upload target for docs

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ copyright = u'2011, Willow Garage'
 # built documents.
 #
 # The short X.Y version.
-version = version[0:3]
+version = version
 # The full version, including alpha/beta/rc tags.
 release = version
 
